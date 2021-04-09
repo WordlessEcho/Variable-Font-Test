8d543a6c6fa18c264e137cd5c28240c70aa760d8
Wordless Echo <wordless@echo.moe>
2021-04-09 20:55:08 +0800

    first commit

---
+++ app/src/main/java/moe/echo/variablefonttest/MainActivity.kt
@@ -0,0 +1,32 @@
+package moe.echo.variablefonttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.slider.Slider
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val previewContent = findViewById<EditText>(R.id.previewContent)
+        val textSize = findViewById<EditText>(R.id.textSize)
+        val wght = findViewById<Slider>(R.id.wght)
+        val preview = findViewById<TextView>(R.id.preview)
+
+        previewContent.addTextChangedListener { text ->
+            preview.text = text
+        }
+
+        textSize.addTextChangedListener { text ->
+            preview.textSize = if (text.toString().isEmpty()) 21.toFloat() else text.toString().toFloat()
+        }
+
+        wght.addOnChangeListener { _, value, _ ->
+            preview.fontVariationSettings = "'wght' $value"
+        }
+    }
+}
