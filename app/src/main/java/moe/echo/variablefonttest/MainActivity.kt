70a19824fc5a220943da8e7a77656c069206d437
WordlessEcho <wordless@echo.moe>
2021-10-30 02:35:31 +0800

    Add font family switcher

--- app/src/main/java/moe/echo/variablefonttest/MainActivity.kt
+++ app/src/main/java/moe/echo/variablefonttest/MainActivity.kt
@@ -1,24 +1,32 @@
 package moe.echo.variablefonttest
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.Switch
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.widget.SwitchCompat
 import androidx.core.widget.addTextChangedListener
 import com.google.android.material.slider.Slider
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+//    should be same order as res/values/font_families_array.xml
+    private val fontFamilyList = arrayOf(
+        Typeface.DEFAULT, Typeface.DEFAULT_BOLD, Typeface.MONOSPACE,
+        Typeface.SANS_SERIF, Typeface.SERIF
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val previewContent = findViewById<EditText>(R.id.previewContent)
-        val textSize = findViewById<EditText>(R.id.textSize)
-        val chws = findViewById<SwitchCompat>(R.id.chws)
-        val wght = findViewById<Slider>(R.id.wght)
-        val preview = findViewById<TextView>(R.id.preview)
+        val previewContent: EditText = findViewById(R.id.previewContent)
+        val textSize: EditText = findViewById(R.id.textSize)
+        val chws: SwitchCompat = findViewById(R.id.chws)
+        val wght: Slider = findViewById(R.id.wght)
+        val preview: TextView = findViewById(R.id.preview)
+        val fontFamilies: Spinner = findViewById(R.id.fontFamilies)
 
         previewContent.addTextChangedListener { text ->
             preview.text = text
@@ -35,5 +43,25 @@ class MainActivity : AppCompatActivity() {
         chws.setOnCheckedChangeListener { buttonView, isChecked ->
             preview.fontFeatureSettings = if (isChecked) "'chws' 1" else ""
         }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.font_families_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            fontFamilies.adapter = adapter
+        }
+
+        fontFamilies.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+        val preview: TextView = findViewById(R.id.preview)
+        preview.typeface = fontFamilyList[pos]
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        Toast.makeText(this, "Nothing selected", Toast.LENGTH_SHORT).show()
     }
 }
