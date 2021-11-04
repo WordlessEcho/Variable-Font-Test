2a6569c72b7e52d5d72091f129ed47a4199242f0
WordlessEcho <wordless@echo.moe>
2021-11-04 15:46:15 +0800

    Use expand feature that provided by Preference

--- app/src/main/java/moe/echo/variablefonttest/PreviewFragment.kt
+++ app/src/main/java/moe/echo/variablefonttest/PreviewFragment.kt
@@ -1,27 +1,6 @@
 package moe.echo.variablefonttest
 
-import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 
 class PreviewFragment: Fragment(R.layout.preview_fragment) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val toggleOptions: ImageButton = view.findViewById(R.id.toggle_options)
-        val options: View = view.findViewById(R.id.options_fragment)
-
-        toggleOptions.setOnClickListener {
-            options.apply {
-                visibility = if (visibility == View.GONE) {
-                    toggleOptions.setImageResource(R.drawable.ic_baseline_expand_less_24)
-                    View.VISIBLE
-                } else {
-                    toggleOptions.setImageResource(R.drawable.ic_baseline_expand_more_24)
-                    View.GONE
-                }
-            }
-        }
-    }
 }
