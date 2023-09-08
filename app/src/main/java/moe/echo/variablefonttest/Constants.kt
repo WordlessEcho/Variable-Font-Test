a5f70d99262102b59427c0597579a4f93daa3106
WordlessEcho <wordless@echo.moe>
2023-09-09 00:20:13 +0800

    Rename preference keys

--- app/src/main/java/moe/echo/variablefonttest/Constants.kt
+++ app/src/main/java/moe/echo/variablefonttest/Constants.kt
@@ -16,19 +16,19 @@ object Constants {
     const val VARIATION_AXIS_WEIGHT = "wght"
 
     const val PREF_CATEGORY_FONT_FEATURES = "fontFeatures"
-    const val FEATURE_CHWS = "chws"
+    const val FEATURE_CHWS = "chwsFeature"
 
     const val PREF_UNSUPPORTED_ANDROID = "unsupportedAndroid"
-    const val PREF_VARIATION_ITALIC = VARIATION_AXIS_ITALIC
-    const val PREF_VARIATION_OPTICAL_SIZE = VARIATION_AXIS_OPTICAL_SIZE
-    const val PREF_VARIATION_SLANT = VARIATION_AXIS_SLANT
-    const val PREF_VARIATION_WIDTH = VARIATION_AXIS_WIDTH
-    const val PREF_VARIATION_WEIGHT = VARIATION_AXIS_WEIGHT
+    const val PREF_VARIATION_ITALIC = "${VARIATION_AXIS_ITALIC}Axis"
+    const val PREF_VARIATION_OPTICAL_SIZE = "${VARIATION_AXIS_OPTICAL_SIZE}Axis"
+    const val PREF_VARIATION_SLANT = "${VARIATION_AXIS_SLANT}Axis"
+    const val PREF_VARIATION_WIDTH = "${VARIATION_AXIS_WIDTH}Axis"
+    const val PREF_VARIATION_WEIGHT = "${VARIATION_AXIS_WEIGHT}Axis"
     const val PREF_VARIATION_EDITOR = "variationEditor"
     const val PREF_ADD_FONT_VARIATION = "addVariation"
     const val PREF_EDIT_VARIATION = "editVariation"
 
-    const val PREF_FEATURE_CHWS = FEATURE_CHWS
+    const val PREF_FEATURE_CHWS = "${FEATURE_CHWS}Feature"
     const val PREF_FEATURE_EDITOR = "featureEditor"
     const val PREF_ADD_FONT_FEATURE = "addFeature"
     const val PREF_EDIT_FEATURE = "editFeature"
